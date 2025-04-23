f02a20fe574c7e2ed63b6dd8fbe1334aadd62a77
Koteka74 <133244579+Koteka74@users.noreply.github.com>
2025-04-24 00:53:35 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  const url = 'https://script.google.com/macros/s/AKfycbwchcqYjRgCxKsOJZ213lIt0YE9UswGkvHkxM9NwGFEJAHaEKJNf2UNJCFYkfbOXQzv4A/exec'; // Ganti ini
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `action=login&username=${username}&password=${password}`,
+
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.success) {
+        window.location.href = "dashboard.html";
+      } else {
+        document.getElementById("loginMessage").classList.remove("hidden");
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      document.getElementById("loginMessage").classList.remove("hidden");
+    });
+});
