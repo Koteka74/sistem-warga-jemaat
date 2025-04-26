ff99b036172496d6faf1c9d52bf549b001b58671
Koteka74 <133244579+Koteka74@users.noreply.github.com>
2025-04-26 13:29:55 +0900

    Update script.js update tanggal

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  const url = 'https://script.google.com/macros/s/AKfycby-cHCD5vBIZ_LqzyH9ednNnqkZZyQLGiDE0EcRASvaJwN-P1xd9eNe3Ec6i2EPf8YCeg/exec'; // Ganti ini
+  const url = 'https://script.google.com/macros/s/AKfycbyJ2GmeDwmWW1Sstso3n0kr4Nvjap5dQX7O_Eu18zi0EAP5irtj4FgjtKUthTvOnSSrWA/exec'; // Ganti ini
 
   fetch(url, {
     method: 'POST',
