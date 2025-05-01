c9a8069e7686fea65c1d67a663ed1ce0bd26ea8d
Koteka74 <133244579+Koteka74@users.noreply.github.com>
2025-05-02 01:24:22 +0900

    Update delete.js update deploy

--- api/delete.js
+++ api/delete.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const scriptURL = "https://script.google.com/macros/s/AKfycbykT3JLLiUMSTvQhSQ9mlsXvLz9frUY1gmS-1FxJ1dUxkdYeqmOxqC0bYPGCEywddjywg/exec";
+  const scriptURL = "https://script.google.com/macros/s/AKfycbxMhJV4b4ZxYG2K0O-FALGbcYxGaIiqpq1aGgc-iINAxJtoZcJLuZ_4h0iMJXHEtp5-/exec";
 
   if (req.method !== "POST") {
     return res.status(405).end("Method Not Allowed");
