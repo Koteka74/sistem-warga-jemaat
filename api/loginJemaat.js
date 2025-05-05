80487f8a8ad98c1706ea9620e4b205989d7444fd
Koteka74 <133244579+Koteka74@users.noreply.github.com>
2025-05-05 18:11:16 +0900

    Update loginJemaat.js new deploy

--- api/loginJemaat.js
+++ api/loginJemaat.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      "https://script.google.com/macros/s/AKfycbw3-XllLxdu01cagX_pCQ_jGnTrCoTsMCbLoS_8l8wYKZGXiyWzEyk_TUSsMKaQvZfxWw/exec?action=loginJemaat",
+      "https://script.google.com/macros/s/AKfycby9w70psh_Xp_lsCmglN6x7PTxr1-VBo-UvXLknDgmPAmlcXhPzdujO2ExXOKIRAinJSg/exec?action=loginJemaat",
       {
         method: "POST",
         body: JSON.stringify({ nama, ortu, rayon }),
