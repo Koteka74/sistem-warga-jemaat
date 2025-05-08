11dfeae9c8c2b0ff318db4ad46904095e296633d
Koteka74 <133244579+Koteka74@users.noreply.github.com>
2025-05-08 13:55:19 +0900

    Update script.js new deploy

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  const url = 'https://script.google.com/macros/s/AKfycby9w70psh_Xp_lsCmglN6x7PTxr1-VBo-UvXLknDgmPAmlcXhPzdujO2ExXOKIRAinJSg/exec'; // Ganti ini
+  const url = 'https://script.google.com/macros/s/AKfycby294uq0SODlKiwTl3qNx8A7j3ugAleXUi2rK6uBGA-PW3JkxhNa_oQQH3Qv9oTbStBgg/exec'; // Ganti ini
 
   fetch(url, {
     method: 'POST',
